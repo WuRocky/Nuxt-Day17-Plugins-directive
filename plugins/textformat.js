831ea9ba98d856962bed49ab8d9a09ea31590924
Rocky Wu <rocky_wu@everrich.com.tw>
2024-11-26 14:59:42 +0800

    Day17-homework

--- plugins/textformat.js
+++ plugins/textformat.js
@@ -1,1 +1,12 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.directive("textformat", (el, binding) => {
+        const {value, arg} = binding
+    
+        const formatMethods = {
+            uppercase: () => value.toUpperCase(),
+            lowercase: () => value.toLowerCase(),
+        }
+
+        el.innerText = formatMethods[arg]() || value;
+    })
+});
--- plugins/timeformat.js
+++ plugins/timeformat.js
@@ -1,1 +1,17 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+function formatTime(value, formatType = "datetime") {
+        const options = {
+        date: { year: "numeric", month: "2-digit", day: "2-digit" },
+        time: { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false },
+        datetime: { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false },
+        };
+    
+        return new Date(value)
+        .toLocaleString("zh-Hant", options[formatType] || options.datetime)
+        .replaceAll("/", "-");
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.directive("timeformat", (el, binding) => {
+        el.innerText = formatTime(binding.value, binding.arg);
+    });
+});
